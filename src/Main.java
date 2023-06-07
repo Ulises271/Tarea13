103755d0670b7c88cf862746e791420735982436
Ulises Muñoz <munozcanoadolfoulises@gmail.com>
2023-06-07 14:32:14 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        VentanaSwing ventanaSwing = new VentanaSwing("Centrigrados a Farenheid");
+        System.out.println("Hello world!");
+    }
+}
